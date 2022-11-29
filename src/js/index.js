ee06d2faeaba935adba61e21ef6812db4283686f
bogusheva <110731443+bogusheva@users.noreply.github.com>
2022-11-30 00:20:41 +0200

    Mobile version

--- src/js/index.js
+++ src/js/index.js
@@ -4,3 +4,17 @@ const header = document.querySelector(".js-header-menu");
 burger.addEventListener("click", () => {
   header.classList.toggle("active");
 });
+
+window.onscroll = function () {
+  myFunction();
+};
+
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
