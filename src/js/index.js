d0d5c6dbb9b4b7a41e651600fca76115dcadac58
bogusheva <110731443+bogusheva@users.noreply.github.com>
2022-12-01 22:48:52 +0200

    Finish responsive design

--- src/js/index.js
+++ src/js/index.js
@@ -4,17 +4,3 @@ const header = document.querySelector(".js-header-menu");
 burger.addEventListener("click", () => {
   header.classList.toggle("active");
 });
-
-window.onscroll = function () {
-  myFunction();
-};
-
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
